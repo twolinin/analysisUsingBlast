228afd8ca5e8ca465fa8edf60ce27a412738fc21
twolinin <twolinin@gmail.com>
2017-04-26 21:42:22 +0800

    backup

--- analysisUsingBlast.cpp
+++ analysisUsingBlast.cpp
@@ -67,30 +67,32 @@ int main(int argc, char** argv)
          inputFile >> refEnd;
          inputFile >> pValue;
          inputFile >> unknown3;
-        /*
-         std::cout<< queryName  << "\t" 
-                  << alignLen   << "\t"
-                  << queryStart << "\t" 
-                  << queryEnd   << "\t" 
-                  << refStart   << "\t"
-                  << refEnd     << "\n";
-         */ 
-        /*
-        std::cout<< queryName  << "\t"
-                 << refName    << "\t"
-                 << bitScore   << "\t"
-                 << alignLen   << "\t"
-                 << unknown1   << "\t"
-                 << unknown2   << "\t"
-                 << queryStart << "\t"
-                 << queryEnd   << "\t"
-                 << refStart   << "\t"
-                 << refEnd     << "\t"
-                 << pValue     << "\t"
-                 << unknown3   << "\t"
-                 << "\n";
+		 /*
+        if( queryName == "tig00000142")
+		
+			 std::cout<< queryName  << "\t" 
+					  << alignLen   << "\t"
+					  << queryStart << "\t" 
+					  << queryEnd   << "\t" 
+					  << refStart   << "\t"
+					  << refEnd     << "\n";
+		*/
+		/*
+			std::cout<< queryName  << "\t"
+					 << refName    << "\t"
+					 << bitScore   << "\t"
+					 << alignLen   << "\t"
+					 << unknown1   << "\t"
+					 << unknown2   << "\t"
+					 << queryStart << "\t"
+					 << queryEnd   << "\t"
+					 << refStart   << "\t"
+					 << refEnd     << "\t"
+					 << pValue     << "\t"
+					 << unknown3   << "\t"
+					 << "\n";
         */
-        if( pValue != 0 || alignLen < 300 )continue;
+        if( alignLen < 300 )continue;
         
         if( alignData.size() == 0 )
         {
@@ -114,7 +116,7 @@ int main(int argc, char** argv)
                  << pValue     << "\t"
                  << unknown3   << "\t"
                  << "\n";
-                 */
+             */    
         }
         else
         {
@@ -180,7 +182,7 @@ int main(int argc, char** argv)
                                  << pValue     << "\t"
                                  << unknown3   << "\t"
                                  << "\n";
-                                 */
+                         */        
                     }
                 }
             }
@@ -219,7 +221,7 @@ int main(int argc, char** argv)
     // filter duplicate
     for(AlignVec::iterator iter = alignData.begin() ; iter != alignData.end() ; ++iter)
     {
-        int max = 0;
+		int max = 0;
         bool* bitArray;
         bool insert = false;
         
@@ -231,7 +233,7 @@ int main(int argc, char** argv)
             resultData.push_back((*iter));
             continue;
         }
-        
+
         for(RangeVec::iterator posIter = (*iter).PBVec.begin() ; posIter != (*iter).PBVec.end() ; ++posIter)
             if( (*posIter).second > max ) max = (*posIter).second;
         
@@ -242,7 +244,7 @@ int main(int argc, char** argv)
         
         for(RangeVec::iterator posIter = (*iter).PBVec.begin() ; posIter != (*iter).PBVec.end() ; ++posIter)
         {
-            int overlap = 0;
+			int overlap = 0;
             int noOverlap = 0;
             bool filter = false;
             
@@ -255,7 +257,7 @@ int main(int argc, char** argv)
                 }
                 else overlap++;
             }
-            
+			
             if( overlap == 0 ) 
             {
                 if(!insert)
@@ -280,9 +282,8 @@ int main(int argc, char** argv)
                     
                 }
             }
-            else if( (float)noOverlap / (float)(overlap + noOverlap ) > 0.7 )
+            else if( (float)noOverlap / (float)(overlap + noOverlap ) > 0.5  || noOverlap > 5000 )
             {
-
                 if(!insert)
                 {
                     alignInfo tmp;
@@ -315,25 +316,30 @@ int main(int argc, char** argv)
 	
 	totalAlignContig = resultData.size();
 	
+	bool showDebug = false;
+	
     for(AlignVec::iterator iter = resultData.begin() ; iter != resultData.end() ; ++iter)
     {
         vector<int>::iterator alnter  = (*iter).alignLenVec.begin(); // record align length
         RangeVec::iterator firPBIter  = (*iter).PBVec.begin();  //record pb align start and end
         RangeVec::iterator firRefIter = (*iter).refVec.begin(); //record ref align start and end
         int positionArray[(*iter).PBVec.size()][4];
-        
+		
         if((*iter).PBVec.size()==1)
         {
             /*
-            cout<< (*iter).contig       << "\t"
-                << (*firPBIter).first   << "\t" 
-                << (*firPBIter).second  << "\t" 
-                << (*firRefIter).first  << "\t" 
-                << (*firRefIter).second << "\t" ;
+			if(showDebug)
+				cout<< (*iter).contig       << "\t"
+					<< (*firPBIter).first   << "\t" 
+					<< (*firPBIter).second  << "\t" 
+					<< (*firRefIter).first  << "\t" 
+					<< (*firRefIter).second << "\t" ;
             */
             alignLength[alignPoint] = (*alnter);
             totalLength += alignLength[alignPoint];
-            //cout<< alignLength[alignPoint] << "\n";
+			
+            //if(showDebug)cout<< alignLength[alignPoint] << "\n";
+			
             alignPoint++;
             continue;
         }
@@ -362,18 +368,19 @@ int main(int argc, char** argv)
 		
         for(int i = 0 ; i < (*iter).PBVec.size() ; i++ )
         {
-            /*
-            std::cout << (*iter).contig      << "\t"
-                      << positionArray[i][0] << "\t"
-                      << positionArray[i][1] << "\t"
-                      << positionArray[i][2] << "\t"
-                      << positionArray[i][3] << "\t"
-                      << "\t";
-            */
+            if(showDebug)
+				std::cout << (*iter).contig      << "\t"
+						  << positionArray[i][0] << "\t"
+						  << positionArray[i][1] << "\t"
+						  << positionArray[i][2] << "\t"
+						  << positionArray[i][3] << "\t"
+						  << "\t";
+            
             if( i == 0 )
             {
                 bool  nowDir     = positionArray[i][2]   > positionArray[i][3];
-                //cout << ( nowDir ? "   <--" : "-->" ) << "\n";
+				
+                if(showDebug)cout << ( nowDir ? "   <--" : "-->" ) << "\n";
                 
                 startPacbioPosition      = positionArray[i][0];
                 referenceAccumulationLen = abs( positionArray[i][2] - positionArray[i][3] );
@@ -381,20 +388,27 @@ int main(int argc, char** argv)
 				continue;
             }
             
-            
             int   pbLen      = abs( startPacbioPosition - positionArray[i][1] );
+			int   pbGapLen   = abs( positionArray[i-1][1] - positionArray[i][0] );
             int   refLen     = abs( positionArray[i][2] - positionArray[i][3] ) + referenceAccumulationLen;
             float dissimilar = (float)((int)( abs( 1 - (float)pbLen/(float)refLen )*1000 ))/1000;
             bool  beforeDir  = positionArray[i-1][2] > positionArray[i-1][3];
             bool  nowDir     = positionArray[i][2]   > positionArray[i][3];
             bool  connect    = ( positionArray[i-1][3] < 10 || positionArray[i][2] < 10 ) || 
-                               ( abs( 1 - (float)positionArray[i-1][3]/(float)positionArray[i][2]) < 0.2 );
+							   ( abs( positionArray[i-1][3] - positionArray[i][2]) < 3000  + pbGapLen );
+                               //( abs( 1 - (float)positionArray[i-1][3]/(float)positionArray[i][2]) < 0.2 );
 
-            //cout << ( nowDir ? "   <--" : "-->" ) << "\t" << dissimilar  << "\t" ;
+			if( positionArray[i-1][2] >= positionArray[i][2] && positionArray[i-1][3] <= positionArray[i][3] ) connect = false;
+			if( positionArray[i][2] >= positionArray[i-1][2] && positionArray[i][3] <= positionArray[i-1][3] ) connect = false;
+			
+            if(showDebug)
+				cout << ( nowDir ? "   <--" : "-->" ) << "\t" 
+				     << positionArray[i-1][1] - positionArray[i][0] << "\t"
+					 << dissimilar  << "\t" ;
                                
             if ( beforeDir == nowDir && dissimilar < 0.2 && connect)
             {
-                //cout << "\n";
+                if(showDebug)cout << "\n";
                 
 				continueMisassembled--;
 				if( continueMisassembled < 0 ) continueMisassembled = 0;
@@ -404,7 +418,7 @@ int main(int argc, char** argv)
             }
             else
             {
-                //cout << referenceAccumulationLen << "\t";
+                if(showDebug)cout << referenceAccumulationLen << "\t";
                 
                 totalLength += referenceAccumulationLen;
                 
@@ -421,16 +435,18 @@ int main(int argc, char** argv)
 				
 				misassembledContigStatus = true;
 				
-				//cout << misassembled << "\t" << "\t" << "\n";
+				if(showDebug)cout << misassembled << "\t" << "\t" << "\n";
 				
             }
 
         }
         
-        
-		if( misassembledContigStatus ) misassembledContig++;
-        
-        //cout << referenceAccumulationLen << "\t" << misassembled << "\t" << "\n\n";
+		if( misassembledContigStatus ) 
+		{
+			//cout << (*iter).contig << "\n";
+			misassembledContig++;
+        }
+        if(showDebug)cout << referenceAccumulationLen << "\t" << misassembled << "\t" << "\n\n";
         alignLength[alignPoint] = referenceAccumulationLen;
         totalLength += referenceAccumulationLen;
         alignPoint++;
